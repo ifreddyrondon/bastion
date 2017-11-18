fa3385059891341559ed16dd108d832f9b8026da
Freddy Rondon <ifreddyrondon@gmail.com>
2017-11-18 15:52:11 -0300

    doc: add colon after name method signature

--- handler.go
+++ handler.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-// ReadJSON parses the JSON-encoded reader and stores the result in the model.
+// ReadJSON, parses the JSON-encoded reader and stores the result in the model.
 func ReadJSON(reader io.ReadCloser, model interface{}) error {
 	decoder := json.NewDecoder(reader)
 	if err := decoder.Decode(model); err != nil {
@@ -16,24 +16,24 @@ func ReadJSON(reader io.ReadCloser, model interface{}) error {
 	return nil
 }
 
-// ResponseJson send a JSON-encoded response in the body of a request with the HTTP status code.
+// ResponseJson, send a JSON-encoded response in the body of a request with the HTTP status code.
 func ResponseJson(w http.ResponseWriter, code int, response interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(response)
 }
 
-// ResponseJson send a JSON-encoded response in the body of a request with the 200 status code.
+// ResponseJson, send a JSON-encoded response in the body of a request with the 200 status code.
 func Send(w http.ResponseWriter, response interface{}) {
 	ResponseJson(w, http.StatusOK, response)
 }
 
-// Created send a JSON-encoded response in the body of a request with the 201 status code.
+// Created, send a JSON-encoded response in the body of a request with the 201 status code.
 func Created(w http.ResponseWriter, response interface{}) {
 	ResponseJson(w, http.StatusCreated, response)
 }
 
-// NoContent send a response without no content with the 204 status code.
+// NoContent, send a response without no content with the 204 status code.
 func NoContent(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNoContent)
 }
