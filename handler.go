1f063289664c7af04505151dea33e188cf9ecb79
Freddy Rondon <ifreddyrondon@gmail.com>
2017-11-18 14:32:11 -0300

    doc: add ReadJSON doc

--- handler.go
+++ handler.go
@@ -5,6 +5,7 @@ import (
 	"io"
 )
 
+// ReadJSON parses the JSON-encoded reader and stores the result in the model.
 func ReadJSON(reader io.ReadCloser, model interface{}) error {
 	decoder := json.NewDecoder(reader)
 	if err := decoder.Decode(model); err != nil {
