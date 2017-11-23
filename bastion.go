8e4ab7089dc0c0a0c1e33db257939c37c6a35642
Freddy Rondon <ifreddyrondon@gmail.com>
2017-11-23 00:21:00 -0300

    feat: add APIRouter with example

--- bastion.go
+++ bastion.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 // Bastion offers an "augmented" Router instance.
@@ -12,24 +13,40 @@ import (
 // Allows to have commons handlers and middleware between projects with the need for each one to do so.
 // Mounted Routers
 // It use go-chi router to modularize the applications. Each instance of GogApp, will have the possibility
-// of mounting an api router, it will define the routes and middleware of the application with the app logic.
+// of mounting an API router, it will define the routes and middleware of the application with the app logic.
 type Bastion struct {
-	Router *chi.Mux
+	r         *chi.Mux
+	APIRouter chi.Router
 }
 
 // NewRouter returns a new GogApp instance ready
 func NewBastion() *Bastion {
 	app := new(Bastion)
-	app.Router = chi.NewRouter()
-	initialize(app.Router)
+	initialize(app)
 	return app
-}
-
-func initialize(r *chi.Mux) {
-	r.Get("/ping", pingHandler)
 }
 
 func (app *Bastion) Run(address string) {
 	log.Printf("Running on %s", address)
-	log.Fatal(http.ListenAndServe(address, app.Router))
+	log.Fatal(http.ListenAndServe(address, app.r))
+}
+
+func initialize(app *Bastion) {
+	/**
+	 * internal router
+	 */
+	app.r = chi.NewRouter()
+
+	/**
+	 * Ping route
+	 */
+	app.r.Get("/ping", pingHandler)
+
+	/**
+	 * API Router
+	 */
+	app.APIRouter = chi.NewRouter()
+	app.APIRouter.Use(middleware.RequestID)
+	app.APIRouter.Use(middleware.Logger)
+	app.r.Mount("/", app.APIRouter)
 }
---
+++ example/helloworld/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ifreddyrondon/gobastion"
+	"github.com/ifreddyrondon/gobastion/utils"
+)
+
+var bastion *gobastion.Bastion
+
+func helloHandler(w http.ResponseWriter, _ *http.Request) {
+	res := struct {
+		Message string
+	}{"world"}
+	utils.Send(w, res)
+}
+
+func main() {
+	bastion = gobastion.NewBastion()
+	bastion.APIRouter.Get("/hello", helloHandler)
+	bastion.Run(":8080")
+}
